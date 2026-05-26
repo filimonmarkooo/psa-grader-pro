a598305074f969d0cdcba37aa5934d7e5bc77b80
filimonmarkooo <filimonmarkooo@gmail.com>
2026-05-26 18:45:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
+app.use(express.static(path.join(__dirname)));
+
+app.all('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-  const indexPath = path.join(__dirname, 'index.html');
-  if (fs.existsSync(indexPath)) {
-    res.sendFile(indexPath);
-  } else {
-    res.send('<h1>App Loading...</h1><p>index.html not found</p>');
-  }
-});
-
-app.get('*', (req, res) => {
-  const indexPath = path.join(__dirname, 'index.html');
-  if (fs.existsSync(indexPath)) {
-    res.sendFile(indexPath);
-  } else {
-    res.send('<h1>App Loading...</h1><p>index.html not found</p>');
-  }
-});
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`PSA Grader running on port ${PORT}`);
 });
