b791652e7181826e0c43793330f91511c2894257
filimonmarkooo <filimonmarkooo@gmail.com>
2026-05-26 18:53:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,31 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve all static files
 app.use(express.static(path.join(__dirname)));
 
-// Serve index.html for any route
-app.use((req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log('Server running');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Listening on ${PORT}`);
+});
+
+// Prevent crashes
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+process.on('uncaughtException', (error) => {
+  console.log('Caught exception: ', error);
+  process.exit(1);
+});
+
+// Keep alive
+setInterval(() => {
+  console.log('Alive');
+}, 30000);
