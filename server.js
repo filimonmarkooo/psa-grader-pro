029260fab6c6747b5704b32b797f5214970668a1
filimonmarkooo <filimonmarkooo@gmail.com>
2026-05-26 18:41:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,23 @@ const app = express();
 app.use(express.static(path.join(__dirname)));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  try {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  } catch (err) {
+    res.status(500).send('Error loading app');
+  }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err);
+});
